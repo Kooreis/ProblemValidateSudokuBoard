89a4d004e3cf676ecd6dae8c72bcaeb95ed4698a
Kooreis <kooreis.official@gmail.com>
2024-03-27 21:24:21 -0500

    This update includes the main method that initializes a Sudoku board and calls the isValidSudoku method to check if the board is valid.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that validates a Sudoku board:
-
-```java
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,29 +17,3 @@ public class Main {
 
         System.out.println(isValidSudoku(board));
     }
-
-    public static boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            Set<Character> rows = new HashSet<>();
-            Set<Character> columns = new HashSet<>();
-            Set<Character> cube = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.' && !rows.add(board[i][j])) {
-                    return false;
-                }
-                if (board[j][i] != '.' && !columns.add(board[j][i])) {
-                    return false;
-                }
-                int rowIndex = 3 * (i / 3);
-                int colIndex = 3 * (i % 3);
-                if (board[rowIndex + j / 3][colIndex + j % 3] != '.' && !cube.add(board[rowIndex + j / 3][colIndex + j % 3])) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-}
-```
-
-This program checks each row, column, and 3x3 grid in the Sudoku board to ensure that no number is repeated. If a number is found to be repeated in any of these, the program returns false, indicating that the Sudoku board is not valid. If no repetitions are found, the program returns true, indicating that the Sudoku board is valid.
