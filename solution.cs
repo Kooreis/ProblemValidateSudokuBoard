8a82a27b3ba3581a38e57f89304fbd9104fbb4ba
Kooreis <kooreis.official@gmail.com>
2024-03-28 22:24:21 -0500

    This update adds the IsValidSudoku function, which checks if the Sudoku board is valid by checking each row, column, and 3x3 box.

--- solution.cs
+++ solution.cs
@@ -1,22 +1,23 @@
-using System;
-using System.Linq;
-
-class Program
+static bool IsValidSudoku(int[,] board)
 {
-    static void Main(string[] args)
+    for (int i = 0; i < 9; i++)
     {
-        int[,] board = new int[9, 9]
+        if (!IsValidRow(board, i) || !IsValidColumn(board, i))
         {
-            {5, 3, 4, 6, 7, 8, 9, 1, 2},
-            {6, 7, 2, 1, 9, 5, 3, 4, 8},
-            {1, 9, 8, 3, 4, 2, 5, 6, 7},
-            {8, 5, 9, 7, 6, 1, 4, 2, 3},
-            {4, 2, 6, 8, 5, 3, 7, 9, 1},
-            {7, 1, 3, 9, 2, 4, 8, 5, 6},
-            {9, 6, 1, 5, 3, 7, 2, 8, 4},
-            {2, 8, 7, 4, 1, 9, 6, 3, 5},
-            {3, 4, 5, 2, 8, 6, 1, 7, 9}
-        };
-
-        Console.WriteLine(IsValidSudoku(board) ? "Valid" : "Invalid");
+            return false;
+        }
     }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (!IsValidBox(board, i * 3, j * 3))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
