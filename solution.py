1ab342af054b291c4872534a176ee8ac4248c99d
Kooreis <kooreis.official@gmail.com>
2024-03-28 22:24:21 -0500

    This function checks if a block (row, column, or 3x3 square) is valid. It filters out empty spaces and then checks if all numbers are unique.

--- solution.py
+++ solution.py
@@ -1,20 +1,3 @@
-def is_valid(board):
-    for i in range(9):
-        row = board[i]
-        if not is_valid_block(row):
-            return False
-
-        column = [board[j][i] for j in range(9)]
-        if not is_valid_block(column):
-            return False
-
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            square = []
-            for x in range(3):
-                for y in range(3):
-                    square.append(board[i+x][j+y])
-            if not is_valid_block(square):
-                return False
-
-    return True
+def is_valid_block(block):
+    filter_block = list(filter(lambda num: num != ".", block))
+    return len(set(filter_block)) == len(filter_block)
